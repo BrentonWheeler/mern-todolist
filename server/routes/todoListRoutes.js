6c6f5912e5f831dcc9f29a06a4b50c693562214e
BrentonWheeler <brenton.wheeler3@gmail.com>
2017-10-22 03:33:54 +1000

    /create route in server/routes/todoListRoutes.js - promises are not working correctly

--- server/routes/todoListRoutes.js
+++ server/routes/todoListRoutes.js
@@ -1,16 +1,49 @@
 var express = require("express");
 //var app = express();
-var UserRouter = new express.Router();
+var TodoListRouter = new express.Router();
 var shortid = require("shortid");
+var Promise = require("promise");
 
 // Require Item model in our routes module
-//var User = require('../models/user');
+var TodoList = require("../models/todoListModels");
 
-// Defined createTodoList route
-UserRouter.route("/create").post(function (req, res) {
-    let tempID = { id: shortid.generate() };
-    console.log(tempID);
-    res.json(tempID);
+// create TodoList route
+TodoListRouter.route("/create").post(function (req, response) {
+    var id = new Promise(function (resolve, reject) {
+        doesntExistInDB("test")
+            .then(res => {
+                console.log("2: " + res);
+                return resolve(res);
+            })
+            .then(res => {
+                console.log("3: " + res);
+            });
+    });
+    let test = id();
+    //console.log(tempID);
+    console.log("4: " + test);
+
+    // USE THIS AFTER PROMISE
+    //console.log(res);
+    //res.json(res);
+
+    // check if short id exists in db
+    // if no continue
+    // if yes re-gen
 });
 
-module.exports = UserRouter;
+// helper functions
+
+// this is recursive
+function doesntExistInDB (tempID) {
+    TodoList.findOne({ id: tempID }, function (err, docs) {
+        if (docs === null) {
+            console.log("1: " + tempID);
+            return Promise.resolve(tempID);
+        } else {
+            //console.log("got here 1 " + tempID);
+            doesntExistInDB(shortid.generate());
+        }
+    });
+}
+module.exports = TodoListRouter;
