f4bf8fbc2672a40a6d3d85a3511847822d5b48dd
BrentonWheeler <brenton.wheeler3@gmail.com>
2017-11-01 20:45:30 +1000

    Heroku testing

--- server/server.js
+++ server/server.js
@@ -3,24 +3,12 @@ var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var cors = require("cors");
-var request = require("request");
 
-request(
-    {
-        url: "https://api.heroku.com/apps/merntodo/config-vars",
-        headers: {
-            Accept: "application/vnd.heroku+json; version=3"
-        }
-    },
-    function (err, res, body) {
-        console.log("test");
-        console.log(body);
-    }
-);
-
-//GET /apps/{app_id_or_name}/config-vars
-
-//var config = require("./config");
+console.log("test below");
+console.log(process.env);
+if (process.env.PRODUCTION === false) {
+    var config = require("./config");
+}
 
 // Route imports
 var indexRoutes = require("./routes/index");
@@ -30,39 +18,39 @@ var todoListRoutes = require("./routes/todoListRoutes");
 // Mongoose connection with mongodb
 mongoose.Promise = require("bluebird");
 
-// if (process.env.PRODUCTION === true) {
-//     mongoose
-//         .connect(
-//             "mongodb://" +
-//                 process.env.mongodb_username +
-//                 ":" +
-//                 process.env.mongodb_password +
-//                 "@ds155634.mlab.com:55634/todolistdb"
-//         )
-//         .then(() => {
-//             console.log("Start");
-//         })
-//         .catch(err => {
-//             console.error("App starting error:", err.stack);
-//             process.exit(1);
-//         });
-// } else {
-//     mongoose
-//         .connect(
-//             "mongodb://" +
-//                 config.mongodb_username +
-//                 ":" +
-//                 config.mongodb_password +
-//                 "@ds155634.mlab.com:55634/todolistdb"
-//         )
-//         .then(() => {
-//             console.log("Start");
-//         })
-//         .catch(err => {
-//             console.error("App starting error:", err.stack);
-//             process.exit(1);
-//         });
-// }
+if (process.env.PRODUCTION === true) {
+    mongoose
+        .connect(
+            "mongodb://" +
+                process.env.mongodb_username +
+                ":" +
+                process.env.mongodb_password +
+                "@ds155634.mlab.com:55634/todolistdb"
+        )
+        .then(() => {
+            console.log("Start");
+        })
+        .catch(err => {
+            console.error("App starting error:", err.stack);
+            process.exit(1);
+        });
+} else {
+    mongoose
+        .connect(
+            "mongodb://" +
+                config.mongodb_username +
+                ":" +
+                config.mongodb_password +
+                "@ds155634.mlab.com:55634/todolistdb"
+        )
+        .then(() => {
+            console.log("Start");
+        })
+        .catch(err => {
+            console.error("App starting error:", err.stack);
+            process.exit(1);
+        });
+}
 
 var app = express();
 
