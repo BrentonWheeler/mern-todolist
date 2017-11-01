365221c78d5851dffcd5bd133ebf51d1aba188ff
BrentonWheeler <brenton.wheeler3@gmail.com>
2017-11-01 19:23:41 +1000

    .

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var cors = require("cors");
 
-if (process.env.PRODUCTION !== true) {
+if (process.env.heroku === false) {
     var config = require("./config");
 }
 
@@ -16,7 +16,7 @@ var todoListRoutes = require("./routes/todoListRoutes");
 // Mongoose connection with mongodb
 mongoose.Promise = require("bluebird");
 
-if (process.env.PRODUCTION === true) {
+if (process.env.heroku === true) {
     mongoose
         .connect(
             "mongodb://" +
