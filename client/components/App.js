9206b36d01b918f566611d6e69e41b1b154d49f8
BrentonWheeler <brenton.wheeler3@gmail.com>
2017-11-02 18:10:42 +1000

    Added on enter functionality for todo item and empty string validation check

--- client/components/App.js
+++ client/components/App.js
@@ -8,20 +8,25 @@ import createTodoItemAction from "../redux/actions/createTodoItemAction";
 class App extends Component {
     constructor (props) {
         super(props);
-        // Temporarily hardcoded base state to check things are working
         this.state = {
             addItemText: ""
         };
         this.addItem = this.addItem.bind(this);
         this.onInputChange = this.onInputChange;
+        this.keyPress = this.keyPress.bind(this);
     }
 
     // Button click handler, adds a new hardcoded item to the this.state.items array
     addItem () {
-        this.props.createTodoItemAction(this.state.addItemText, this.props.match.params.id);
-        this.setState({
-            addItemText: ""
-        });
+        if (this.state.addItemText === "") {
+            // Put a nice UX toast here
+            //window.alert("Missing todo item text");
+        } else {
+            this.props.createTodoItemAction(this.state.addItemText, this.props.match.params.id);
+            this.setState({
+                addItemText: ""
+            });
+        }
     }
 
     // Input onChange handler
@@ -31,13 +36,25 @@ class App extends Component {
         });
     }
 
+    // On enter press
+    keyPress (e) {
+        if (e.keyCode === 13) {
+            this.addItem();
+        }
+    }
+
     render () {
         return (
             <div className="App">
                 <h1>Todolist Unique ID: {this.props.match.params.id}</h1>
                 My Todo List
                 <br />
-                <input onChange={this.onInputChange.bind(this)} value={this.state.addItemText} type="text" />
+                <input
+                    onChange={this.onInputChange.bind(this)}
+                    onKeyDown={this.keyPress}
+                    value={this.state.addItemText}
+                    type="text"
+                />
                 <button type="button" onClick={this.addItem}>
                     Add Item
                 </button>
--- client/components/Login.js
+++ client/components/Login.js
@@ -1,3 +1,5 @@
+// not currently in use
+
 import React, { Component } from "react";
 //import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
